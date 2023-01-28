6bdc7ce49ea11f2aced28be11108dad854fb7b51
usama62 <usamaebrahim95@gmail.com>
2023-01-28 20:04:58 +0500

    home screen initiated

--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'entry_option.dart';
-import 'package:easy_localization/easy_localization.dart';
 
 class Home extends StatefulWidget {
   const Home({
@@ -17,43 +15,139 @@ class _HomeState extends State<Home> {
     return Scaffold(
         body: SingleChildScrollView(
       child: Stack(children: [
-        Container(
-          decoration: const BoxDecoration(
-            image: DecorationImage(
-                image: AssetImage('assets/images/doodle.png'),
-                fit: BoxFit.cover),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.only(top: 100),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Image.asset('assets/images/logo.png'),
-                const Padding(
-                  padding: EdgeInsets.fromLTRB(0.0, 50.0, 0.0, 5.0),
-                  child: Text(
-                    "This is Home Page",
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 30),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Padding(
+                padding: EdgeInsets.fromLTRB(0.0, 0.0, 50.0, 0.0),
+                child: Text("MyStyle",
                     style: TextStyle(
                         fontFamily: "Product Sans",
                         color: Color(0xff000000),
-                        fontSize: 28.0,
-                        fontWeight: FontWeight.bold),
+                        fontSize: 24.0,
+                        fontWeight: FontWeight.bold)),
+              ),
+              Container(
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(20.0),
+                    topRight: Radius.circular(20.0),
+                    bottomLeft: Radius.circular(20.0),
+                    bottomRight: Radius.circular(20.0),
                   ),
+                  color: Color(0xFFFCDFE2),
                 ),
+                child: const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20),
+                  child: Text("Free Trial : 22 days left",
+                      style: TextStyle(
+                          fontFamily: "Product Sans",
+                          color: Color(0xffE60D21),
+                          fontSize: 12.0,
+                          fontWeight: FontWeight.bold)),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(20.0, 0.0, 0.0, 0.0),
+                child: Image.asset("assets/images/messages.png"),
+              )
+            ],
+          ),
+        ),
+        SingleChildScrollView(
+            padding: const EdgeInsets.fromLTRB(0, 80, 0, 0),
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: [
+                Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Column(children: [
+                      Image.asset("assets/images/jennifer.png"),
+                      const Padding(
+                        padding: EdgeInsets.fromLTRB(0, 5, 0, 0),
+                        child: Text("Jennifier",
+                            style: TextStyle(
+                                fontFamily: "Product Sans",
+                                color: Color(0xff000000),
+                                fontSize: 12.0,
+                                fontWeight: FontWeight.bold)),
+                      )
+                    ])),
+                Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Column(children: [
+                      Image.asset("assets/images/jennifer.png"),
+                      const Padding(
+                        padding: EdgeInsets.fromLTRB(0, 5, 0, 0),
+                        child: Text("Jennifier",
+                            style: TextStyle(
+                                fontFamily: "Product Sans",
+                                color: Color(0xff000000),
+                                fontSize: 12.0,
+                                fontWeight: FontWeight.bold)),
+                      )
+                    ])),
+                Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Column(children: [
+                      Image.asset("assets/images/jennifer.png"),
+                      const Padding(
+                        padding: EdgeInsets.fromLTRB(0, 5, 0, 0),
+                        child: Text("Jennifier",
+                            style: TextStyle(
+                                fontFamily: "Product Sans",
+                                color: Color(0xff000000),
+                                fontSize: 12.0,
+                                fontWeight: FontWeight.bold)),
+                      )
+                    ])),
+                Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Column(children: [
+                      Image.asset("assets/images/jennifer.png"),
+                      const Padding(
+                        padding: EdgeInsets.fromLTRB(0, 5, 0, 0),
+                        child: Text("Jennifier",
+                            style: TextStyle(
+                                fontFamily: "Product Sans",
+                                color: Color(0xff000000),
+                                fontSize: 12.0,
+                                fontWeight: FontWeight.bold)),
+                      )
+                    ])),
+                Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Column(children: [
+                      Image.asset("assets/images/jennifer.png"),
+                      const Padding(
+                        padding: EdgeInsets.fromLTRB(0, 5, 0, 0),
+                        child: Text("Jennifier",
+                            style: TextStyle(
+                                fontFamily: "Product Sans",
+                                color: Color(0xff000000),
+                                fontSize: 12.0,
+                                fontWeight: FontWeight.bold)),
+                      )
+                    ])),
+                Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Column(children: [
+                      Image.asset("assets/images/jennifer.png"),
+                      const Padding(
+                        padding: EdgeInsets.fromLTRB(0, 5, 0, 0),
+                        child: Text("Jennifier",
+                            style: TextStyle(
+                                fontFamily: "Product Sans",
+                                color: Color(0xff000000),
+                                fontSize: 12.0,
+                                fontWeight: FontWeight.bold)),
+                      )
+                    ]))
               ],
-            ),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.fromLTRB(20.0, 30.0, 0.0, 0.0),
-          child: IconButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            icon: const Icon(Icons.arrow_back),
-            //replace with our own icon data.
-          ),
-        ),
+            ))
       ]),
     ));
   }
