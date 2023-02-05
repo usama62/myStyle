3c20086e91a3f0541f2891571fa40bae6ee655ca
usama62 <usamaebrahim95@gmail.com>
2023-02-05 22:32:50 +0500

    aaaaaaaa

--- lib/add_card.dart
+++ lib/add_card.dart
@@ -14,6 +14,72 @@ class _AddCardState extends State<AddCard> {
   final TextEditingController _usernameController = TextEditingController();
   bool isChecked = true;
 
+  void _settingModalBottomSheet(context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (BuildContext bc) {
+          return Wrap(
+            children: <Widget>[
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(25, 40, 0, 0),
+                    child: Image.asset("assets/images/add_card_large.png"),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.fromLTRB(25, 25, 0, 0),
+                    child: Text(
+                      "Success",
+                      style: TextStyle(
+                          fontFamily: "Product Sans",
+                          color: Color(0xFF000000),
+                          fontSize: 28.0,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.fromLTRB(25, 10, 0, 0),
+                    child: Text(
+                      "Your card has been added successfully",
+                      style: TextStyle(
+                          fontFamily: "Product Sans",
+                          color: Color(0xFF000000),
+                          fontSize: 18.0,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(25, 10, 25, 40),
+                    child: TextButton(
+                      style: TextButton.styleFrom(
+                          padding: const EdgeInsets.fromLTRB(
+                              131.5, 14.0, 131.5, 14.0),
+                          backgroundColor: const Color(0xFFE60D21),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(25.0),
+                          )),
+                      onPressed: () {},
+                      child: const Center(
+                        child: Text(
+                          "Okay",
+                          style: TextStyle(
+                              fontFamily: "Product Sans",
+                              color: Color(0xFFFFFFFF),
+                              fontSize: 17.0,
+                              fontWeight: FontWeight.normal),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          );
+        });
+  }
+
   @override
   void initState() {
     super.initState();
@@ -311,10 +377,7 @@ class _AddCardState extends State<AddCard> {
                         borderRadius: BorderRadius.circular(25.0),
                       )),
                   onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => const Profile()),
-                    );
+                    _settingModalBottomSheet(context);
                   },
                   child: const Center(
                     child: Text(
